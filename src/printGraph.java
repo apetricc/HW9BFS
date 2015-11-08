d8f1bd5a4140d088c584d072783b207e4770d0b8
Andrew P <apetricc@unca.edu>
2015-11-08 13:11:31 -0500

    Update printGraph.java
    this method needs to go inside the Graph class apparently

--- src/printGraph.java
+++ src/printGraph.java
@@ -1,14 +1,19 @@
+package hw9bfs;
+
 /**
  * Created by petriccione on 11/5/15.
  */
 /*
-  Define a printGraph class method to print n,
+  Define a PrintGraph class method to print n,
   and then a nicely formatted table of the adjacency matrix.
  */
-public class printGraph {
-    public void printGraph(Graph theGraph) {
-        System.out.println("The graph has " + theGraph.getN() + " vertices.");
-        for (int i = 0; i < theGraph.getN(); i++) {
+
+public class PrintGraph {
+    public void printGraph(Graph G) {
+        System.out.println("The graph has " + G.getN() + " vertices.");
+        
+        
+        for (int i = 0; i < G.getN(); i++) {
             for (int j = 0; j < theGraph.getN(); j++) {
                 System.out.println(theGraph.vertices[i][j]);
                 System.out.println(theGraph.edges[i][j]);
